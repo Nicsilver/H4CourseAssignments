aecaa06ea27490c6637c23647765e6f36b6ac36b
Nicsilver <nic.silver@hotmail.dk>
2020-04-17 10:50:40 +0200

    123

---
+++ Encryption/Password/Password/Password/Hash.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Password
+{
+    class Hash
+    {
+        public byte[] GenerateSalt()
+        {
+            int hashByteSize = 32;
+            using (RNGCryptoServiceProvider RNGCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                byte[] salt = new byte[hashByteSize];
+                RNGCryptoServiceProvider.GetBytes(salt);
+
+                return salt;
+            }
+        }
+
+        private byte[] Combine(byte[] first, byte[] second)
+        {
+            var combinedValues = new byte[first.Length + second.Length];
+
+            Buffer.BlockCopy(first, 0, combinedValues, 0, first.Length);
+            Buffer.BlockCopy(second, 0, combinedValues, first.Length, second.Length);
+
+            return combinedValues;
+        }
+
+        //Unsure if this is enough, doesnt use HMAC?
+        public byte[] GenerateHashedPassword(byte[] password, byte[] salt, int iterations, int hashByteSize)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(password, salt, iterations))
+            {
+                return rfc2898.GetBytes(hashByteSize);
+            }
+        }
+
+        public byte[] hashPasswordWithSaltSha256(byte[] password, byte[] salt, int iterations)
+        {
+            using (var shar256 = SHA256.Create())
+            {
+                Console.WriteLine("pre combine" + Convert.ToBase64String(password));
+                byte[] passwordHashed  = Combine(password, salt);
+                Console.WriteLine("post combine" + Convert.ToBase64String(passwordHashed));
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    passwordHashed = shar256.ComputeHash(passwordHashed);
+                }
+                return passwordHashed;
+            }
+        }
+
+    }
+}
---
+++ Encryption/Password/Password/Password/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Password
+{
+
+
+    //MADE WITH TEXT FILES TO BEGIN WITH WILL BE CHANGED TO SQL LATER
+    class Program
+    {
+        
+
+        private static int iterations;
+        private static Hash hash;
+
+        private static int salt;
+        private static byte[] bytearray;
+        
+        static void Main(string[] args)
+        {
+            if (File.Exists("login.txt"))
+            {
+                File.Delete("login.txt");
+            }
+
+            iterations = 1000;
+            hash = new Hash();
+            while (true)
+            {
+                byte[] password = null;
+                Console.WriteLine("Press 1 for create user:\n" +
+                    "Press 2 for login:\n" +
+                    "Press 3: \n" +
+                    "");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Write password");
+                        password = Encoding.ASCII.GetBytes(Console.ReadLine());
+                        createLogin(password);
+
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Write password");
+                        password = Encoding.ASCII.GetBytes(Console.ReadLine());
+                        verifyLogin(password);
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+        }
+
+        static void createLogin(byte[] password)
+        {
+            Console.WriteLine(Convert.ToBase64String(password));
+            byte[] salt = hash.GenerateSalt();
+            byte[] hashedPassword = hash.hashPasswordWithSaltSha256(password, salt, iterations);
+            Console.WriteLine("Salt" + Convert.ToBase64String(salt));
+
+            using (StreamWriter writetext = new StreamWriter("login.txt"))
+            {
+                Console.WriteLine(Convert.ToBase64String(hashedPassword) + " : " + Convert.ToBase64String(salt));
+                writetext.WriteLine(Convert.ToBase64String(hashedPassword));
+                writetext.WriteLine(Convert.ToBase64String(salt));
+            }
+            using (StreamReader streamReader = new StreamReader("login.txt"))
+            {
+                string hashedTest = streamReader.ReadLine();
+                string saltTest = streamReader.ReadLine();
+
+                Console.WriteLine(hashedTest.ToString() + " : " + saltTest.ToString());
+
+            }
+        } 
+
+        static void verifyLogin(byte[] password)
+        {
+            Console.WriteLine(Convert.ToBase64String(password));
+
+            using (StreamReader streamReader = new StreamReader("login.txt"))
+            {
+                string hashedPassword = streamReader.ReadLine();
+                string salt = streamReader.ReadLine();
+                Console.WriteLine("Salt" + salt);
+
+                byte[] reHashedPassword = hash.hashPasswordWithSaltSha256(password, Encoding.ASCII.GetBytes(salt), iterations);
+
+                Console.WriteLine(hashedPassword.ToString() + " : " + salt.ToString());
+
+                Console.WriteLine("hashesPassword: " + hashedPassword);
+                Console.WriteLine("rehashed: " + Convert.ToBase64String(reHashedPassword));
+
+                if (Encoding.ASCII.GetBytes(hashedPassword).Equals(reHashedPassword))
+                {
+                    Console.WriteLine("Logged in");
+                }
+                else
+                {
+                    Console.WriteLine("incorect password");
+                }
+            }
+
+
+        }
+
+
+
+    }
+}
